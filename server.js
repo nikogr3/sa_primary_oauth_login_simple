328b4eeb87549c8425d51b8db91bf1784ed598bf
Niko Theo <niko.th@gmail.com>
2024-05-20 18:33:08 -0700

    add a test route

--- server.js
+++ server.js
@@ -116,6 +116,10 @@ app.get("/token", (req, res) => {
   }
 });
 
+app.get("/test", (req, res) => {
+  res.send("up and running");
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
